0c2a832154fabe5771bd44a7bcf4890df1b5d432
Alen Subasic <alen.subasic@dhigroupinc.com>
2023-10-22 17:09:46 -0500

    additional dynamic vars

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ todays_date = now.strftime("%Y-%m-%d")
 todays_date_eu = now.strftime("%d.%m.%Y")
 
 # Get only current Saturday's video
-PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", now.strftime('%Y-%m-%dT00:00:00Z'))
+PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", "2023-10-20T00:00:00Z") #now.strftime('%Y-%m-%dT00:00:00Z'))
 PUBLISHED_BEFORE =  os.getenv("PUBLISHED_BEFORE", now.strftime(f'{tomorrow}T00:00:00Z'))
 # Show is released weekly
 EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 37)
 # Current year + next year - 4031 gives us this season
 SEASON_NUMBER = year + (year + 1) - 4031
 # YouTube Data API Key
-API_KEY = os.getenv("API_KEY", "")
+API_KEY = os.getenv("API_KEY", "AIzaSyA3K-ylDNYLsxobA2l4_ktrRDV71EdZzoo")
 # Zvezde Granda Channel ID
-CHANNEL_ID = 'UChz9nfVNmUiZryQtekbzS5g'
+CHANNEL_ID = os.getenv("CHANNEL_ID", "UChz9nfVNmUiZryQtekbzS5g")
 # Changes episode number everyweek to match uploaded episode number
-QUERY = f"Zvezde Granda Cela emisija {todays_date_eu}"
+QUERY = os.getenv("QUERY", f"Zvezde Granda Cela emisija {todays_date_eu}")
 # Save as filename
 FILENAME = f"Zvezde Granda - S{SEASON_NUMBER:02}E{EPISODE_NUMBER:02}"
 # Max amount of search resultes from YouTube Search API
@@ -37,9 +37,9 @@ MAX_RESULTS=1
 FORMAT = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
 
 # Display all passed in variables
-print("***********ENV VARIABLES***********")
-for name, value in os.environ.items():
-    print("{0}: {1}".format(name, value))
+# print("***********ENV VARIABLES***********")
+# for name, value in os.environ.items():
+#     print("{0}: {1}".format(name, value))
 
 # Prevent downloading of the same episode
 if os.path.isfile(f"downloads/{FILENAME}"):
