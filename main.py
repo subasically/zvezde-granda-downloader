789466e652d4ce467885cd711cdf655d063501df
Alen Subasic <alensubasic@gmail.com>
2023-10-22 18:41:27 -0500

    fixing episode numbering

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ todays_date_eu = now.strftime("%d.%m.%Y")
 PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", now.strftime('%Y-%m-%dT00:00:00Z'))
 PUBLISHED_BEFORE =  os.getenv("PUBLISHED_BEFORE", now.strftime(f'{tomorrow}T00:00:00Z'))
 # Show is released weekly
-EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 37)
+EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 38)
 # Current year + next year - 4031 gives us this season
 SEASON_NUMBER = year + (year + 1) - 4031
 # YouTube Data API Key
