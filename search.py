8d0ebef8213a8671790b91f712364aed418874cb
Alen Subasic <alen.subasic@dhigroupinc.com>
2023-10-22 17:55:33 -0500

    clean up

--- search.py
+++ search.py
@@ -2,21 +2,24 @@ import sys
 from googleapiclient.discovery import build
 
 def channel_videos(QUERY, API_KEY,CHANNEL_ID, MAX_RESULTS):
-    print("Searching YouTube for new episodes...", QUERY, API_KEY, CHANNEL_ID, MAX_RESULTS)
+    print("Searching for new episodes...", QUERY, CHANNEL_ID)
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     request = youtube.search().list(
         part="snippet",
         q=QUERY,
-        type="video",
+        # type="video",
         maxResults=MAX_RESULTS,
         channelId=CHANNEL_ID,
     )
 
     response = request.execute()
 
+    # print(response)
+
     if not response["items"]:
         print("No new episodes :(")
         sys.exit()
     else:
+        print("New episode found!", response["items"][0]["snippet"]["title"])
         return response
