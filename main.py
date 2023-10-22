6c2f786d91214817a4a511c1e0ede2b49a18d981
Alen Subasic <alen.subasic@dhigroupinc.com>
2023-10-22 17:40:17 -0500

    clean up

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ todays_date = now.strftime("%Y-%m-%d")
 todays_date_eu = now.strftime("%d.%m.%Y")
 
 # Get only current Saturday's video
-PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", "2023-10-20T00:00:00Z") #now.strftime('%Y-%m-%dT00:00:00Z'))
+PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", now.strftime('%Y-%m-%dT00:00:00Z'))
 PUBLISHED_BEFORE =  os.getenv("PUBLISHED_BEFORE", now.strftime(f'{tomorrow}T00:00:00Z'))
 # Show is released weekly
 EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 37)
 # Current year + next year - 4031 gives us this season
 SEASON_NUMBER = year + (year + 1) - 4031
 # YouTube Data API Key
-API_KEY = os.getenv("API_KEY", "AIzaSyA3K-ylDNYLsxobA2l4_ktrRDV71EdZzoo")
+API_KEY = os.getenv("API_KEY", "")
 # Zvezde Granda Channel ID
 CHANNEL_ID = os.getenv("CHANNEL_ID", "UChz9nfVNmUiZryQtekbzS5g")
 # Changes episode number everyweek to match uploaded episode number
