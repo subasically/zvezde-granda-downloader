bbf2d2badfa453fd02f9851c63247020f6970bde
Alen Subasic <alensubasic@gmail.com>
2023-11-04 20:16:31 -0500

    date fix

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ import search
 
 now = datetime.now()
 start_year = 2004
-year = int(now.strftime('%Y'))
-yesterday = (now - timedelta(days = 1)).strftime("%Y-%m-%d")
-tomorrow = (now - timedelta(days = -1)).strftime("%Y-%m-%d")
-week_number = now.strftime('%U')
-todays_day = now.strftime('%A')
+year = int(now.strftime("%Y"))
+yesterday = (now - timedelta(days=1)).strftime("%Y-%m-%d")
+tomorrow = (now - timedelta(days=-1)).strftime("%Y-%m-%d")
+week_number = now.strftime("%U")
+todays_day = now.strftime("%A")
 todays_date = now.strftime("%Y-%m-%d")
 todays_date_eu = now.strftime("%d.%m.%Y")
 
 API_KEY = os.getenv("API_KEY", "")
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK", "")
 # Get only current Saturday's video
-PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", now.strftime('%Y-%m-%dT00:00:00Z'))
-PUBLISHED_BEFORE =  os.getenv("PUBLISHED_BEFORE", now.strftime(f'{tomorrow}T00:00:00Z'))
+# PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", now.strftime('%Y-%m-%dT00:00:00Z'))
+# PUBLISHED_BEFORE =  os.getenv("PUBLISHED_BEFORE", now.strftime(f'{tomorrow}T00:00:00Z'))
 # Show is released weekly
-EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 38)
+EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 37)
 # Current year + next year - 4031 gives us this season
 SEASON_NUMBER = year + (year + 1) - 4031
 # YouTube Data API Key
@@ -33,9 +33,9 @@ QUERY = os.getenv("QUERY", f"Zvezde Granda Cela emisija {todays_date_eu}")
 # Save as filename
 FILENAME = f"Zvezde Granda - S{SEASON_NUMBER:02}E{EPISODE_NUMBER:02}"
 # Max amount of search resultes from YouTube Search API
-MAX_RESULTS=1
+MAX_RESULTS = 1
 # We want MP4 for best playback on Apple devices
-FORMAT = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+FORMAT = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 
 # Display all passed in variables
 # print("***********ENV VARIABLES***********")
@@ -59,7 +59,7 @@ if __name__ == "__main__":
         print("Invalid value!")
 
     try:
-        download.video(search_results, FILENAME, FORMAT, SLACK_WEBHOOK)
-        print('downloading')
+        # download.video(search_results, FILENAME, FORMAT, SLACK_WEBHOOK)
+        print("downloading")
     except ValueError:
         print("Invalid value!")
