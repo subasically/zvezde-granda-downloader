b24a17382950c8ae5e9b61bd6a0ee67678095dba
Alen Subasic <alensubasic@gmail.com>
2023-11-13 10:38:05 -0600

    thumbnail filename fix

--- download.py
+++ download.py
@@ -2,20 +2,18 @@ import yt_dlp
 import save_file
 import notification
 
+
 def video(response, FILENAME, FORMAT, WEBHOOK):
-    ydl_options = {
-        'outtmpl': "downloads/" + FILENAME,
-        'format': FORMAT
-    }
+    ydl_options = {"outtmpl": "downloads/" + FILENAME, "format": FORMAT}
     ydl = yt_dlp.YoutubeDL(ydl_options)
 
     for item in response["items"]:
-        video_url = 'http://www.youtube.com/watch?v=' + item["id"]["videoId"]
+        video_url = "http://www.youtube.com/watch?v=" + item["id"]["videoId"]
         thumbnail_url = item["snippet"]["thumbnails"]["high"]["url"]
         thumb_max_url = thumbnail_url.replace("hqdefault", "maxresdefault")
 
         print(thumb_max_url)
-        save_file.thumbnail(thumb_max_url, FILENAME + ' - Thumbnail.png')
+        save_file.thumbnail(thumb_max_url, "${FILENAME}.png")
 
         ydl.download(video_url)
 
