dde956e0fcebc86e4384e405fbd03f30d97af4b3
Alen Subasic <alensubasic@gmail.com>
2023-11-13 10:14:16 -0600

    updated query to match video date param

--- main.py
+++ main.py
@@ -22,23 +22,12 @@ todays_date_eu = now.strftime("%d.%m.%Y")
 API_KEY = os.getenv("API_KEY", "")
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK", "")
 VIDEO_DATE = os.getenv("VIDEO_DATE", todays_date_eu)
-# Get only current Saturday's video
-# PUBLISHED_AFTER = os.getenv("PUBLISHED_AFTER", now.strftime('%Y-%m-%dT00:00:00Z'))
-# PUBLISHED_BEFORE =  os.getenv("PUBLISHED_BEFORE", now.strftime(f'{tomorrow}T00:00:00Z'))
-# Show is released weekly
 EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 38)
-# Current year + next year - 4031 gives us this season
 SEASON_NUMBER = year + (year + 1) - 4031
-# YouTube Data API Key
-# Zvezde Granda Channel ID
 CHANNEL_ID = os.getenv("CHANNEL_ID", "UChz9nfVNmUiZryQtekbzS5g")
-# Changes episode number everyweek to match uploaded episode number
-QUERY = os.getenv("QUERY", f"Zvezde Granda Cela emisija {todays_date_eu}")
-# Save as filename
+QUERY = os.getenv("QUERY", f"Zvezde Granda Cela emisija {VIDEO_DATE}")
 FILENAME = f"Zvezde Granda - S{SEASON_NUMBER:02}E{EPISODE_NUMBER:02}"
-# Max amount of search resultes from YouTube Search API
 MAX_RESULTS = 1
-# We want MP4 for best playback on Apple devices
 FORMAT = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 
 # Display all passed in variables
