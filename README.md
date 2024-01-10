6dfa6d0a4bf7e06f44b7de69799c977aa6630cff
Alen Subasic <alen.subasic@dhigroupinc.com>
2024-01-09 21:00:46 -0600

    refactor

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 
 Downloads newest episode uploaded.
 
-| parameter     | required | type   | default                                                           |
-| ------------- | -------- | ------ | ----------------------------------------------------------------- |
-| API_KEY       | yes      | string | ""                                                                |
-| SLACK_WEBHOOK | no       | string | ""                                                                |
-| VIDEO_DATE    | no       | string | "DD.MM.YYYY"                                                      |
-| QUERY         | no       | string | "Zvezde Granda - Cela emisija {EPISODE_NUMBER:02} - {VIDEO_DATE}" |
+| parameter          | required | type   | default                                            |
+| ------------------ | -------- | ------ | -------------------------------------------------- |
+| API_KEY            | yes      | string | ""                                                 |
+| START_DATE         | yes      | string | "2023-09-23"                                       |
+| CHANNEL_ID         | yes      | string | ""                                                 |
+| SLACK_WEBHOOK      | no       | string | ""                                                 |
+| VIDEO_DATE         | no       | string | "DD.MM.YYYY"                                       |
+| QUERY              | no       | string | "Zvezde Granda - Cela emisija S15E01 - DD.MM.YYYY" |
+| EPISODE_ADJUSTMENT | no       | string | 1                                                  |
+| EPISODE_NUMBER     | no       | string | "weeks_since_start_date + episode_adjustmen"       |
+| SEASON_NUMBER      | no       | string | "start_date_year - 2008"                           |
 
 ```bash
 docker run --rm \
-    --pull=always \
+    # --pull=always \
     --name="zvezde-granda-downloader" \
     -e API_KEY="***INSERT_API_KEY_HERE***" \
+    -e CHANNEL_ID="***INSERT_YOUTUBE_CHANNEL_ID***" \
     -e SLACK_WEBHOOK="***INSERT_SLACK_WEBHOOK_HERE***" \
-    -e VIDEO_DATE="25.11.2023" \
     -v "/local/folder/show/season/:/usr/src/app/downloads" \
     subasically/zvezde-granda-downloader:main
 ```
--- main.py
+++ main.py
@@ -3,33 +3,52 @@ import sys
 from datetime import datetime, timedelta
 import pytz
 
-tz = pytz.timezone("Europe/Berlin")
-now = tz.localize(datetime.now())
-
-print("Current time:", now)
-
 # Local Modules
 import download
 import search
 
-start_year = 2004
-year = int(now.strftime("%Y"))
-yesterday = (now - timedelta(days=1)).strftime("%Y-%m-%d")
-tomorrow = (now - timedelta(days=-1)).strftime("%Y-%m-%d")
-week_number = now.strftime("%U")
-todays_day = now.strftime("%A")
-todays_date = now.strftime("%Y-%m-%d")
-todays_date_eu = now.strftime("%d.%m.%Y")
+tz = pytz.timezone("Europe/Berlin")
+now = tz.localize(datetime.now())
+now = now.replace(tzinfo=None)  # Convert to naive datetime object
+
+print("Current Time:", now.strftime("%Y-%m-%d %H:%M:%S"))
+
+episode_adjustment = int(os.getenv("EPISODE_ADJUSTMENT", 1))
+start_date = os.getenv("START_DATE", now.strftime("2023-09-23"))  # 2023-09-23
+start_date_obj = datetime.strptime(start_date, "%Y-%m-%d")  # 2023-09-23
+start_date_year = datetime.strptime(start_date, "%Y-%m-%d").year  # 2023
+weeks_since_start = (now - start_date_obj).days // 7  # 0-52
+today = now.strftime("%Y-%m-%d")  # 2023-01-01
+yesterday = (now - timedelta(days=1)).strftime("%Y-%m-%d")  # -1 day
+tomorrow = (now - timedelta(days=-1)).strftime("%Y-%m-%d")  # +1 day
+week_number = now.strftime("%U")  # 01-53
+todays_day = now.strftime("%A")  # Monday
+todays_date = now.strftime("%Y-%m-%d")  # 2023-01-01
+
+if todays_day == "Saturday":
+    print("Saturday")
+    video_date = now.strftime("%d.%m.%Y")  # 31.12.2022
+elif todays_day == "Sunday":
+    print("Sunday")
+    video_date = (now - timedelta(days=1)).strftime("%d.%m.%Y")  # 02.01.2023
+elif todays_day == "Monday":
+    print("Monday")
+    video_date = (now - timedelta(days=2)).strftime("%d.%m.%Y")
+elif todays_day == "Tuesday":
+    print("Tuesday")
+    video_date = (now - timedelta(days=3)).strftime("%d.%m.%Y")
+else:
+    video_date = todays_date.strftime("%d.%m.%Y")
 
 API_KEY = os.getenv("API_KEY", "")
 SLACK_WEBHOOK = os.getenv(
     "SLACK_WEBHOOK",
     "",
 )
-VIDEO_DATE = os.getenv("VIDEO_DATE", todays_date_eu)
-EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", int(week_number) - 37)
-SEASON_NUMBER = year + (year + 1) - 4031
-CHANNEL_ID = os.getenv("CHANNEL_ID", "UChz9nfVNmUiZryQtekbzS5g")
+VIDEO_DATE = os.getenv("VIDEO_DATE", video_date)
+EPISODE_NUMBER = os.getenv("EPISODE_NUMBER", weeks_since_start + episode_adjustment)
+SEASON_NUMBER = os.getenv("SEASON_NUMBER", start_date_year - 2008)
+CHANNEL_ID = os.getenv("CHANNEL_ID", "")
 QUERY = os.getenv(
     "QUERY", f"Zvezde Granda - Cela emisija {EPISODE_NUMBER:02} - {VIDEO_DATE}"
 )
@@ -37,10 +56,7 @@ FILENAME = f"Zvezde Granda - S{SEASON_NUMBER:02}E{EPISODE_NUMBER:02} - {VIDEO_DA
 MAX_RESULTS = 1
 FORMAT = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 
-# Display all passed in variables
-# print("***********ENV VARIABLES***********")
-# for name, value in os.environ.items():
-#     print("{0}: {1}".format(name, value))
+# print(FILENAME)
 
 # Prevent downloading of the same episode
 if os.path.isfile(f"downloads/{FILENAME}"):
@@ -52,7 +68,12 @@ if not API_KEY:
     print("API_KEY invalid or null")
     sys.exit()
 
+if not CHANNEL_ID:
+    print("CHANNEL_ID invalid or null")
+    sys.exit()
+
 if __name__ == "__main__":
+    # print("starting...")
     try:
         search_results = search.channel_videos(QUERY, API_KEY, CHANNEL_ID, MAX_RESULTS)
     except ValueError:
